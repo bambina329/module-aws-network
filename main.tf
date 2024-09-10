3c1fe92f9b9e6ee4a8533345103ae56b17d67173
Khamitov Rinat <hamitovr@gmail.com>
2024-09-10 13:17:44 +0500

    network module create

--- main.tf
+++ main.tf
@@ -104,14 +104,14 @@ resource "aws_route_table_association" "public-b-association" {
 }
 
 resource "aws_eip" "nat-a" {
-  vpc  = true
+  vpc = true
   tags = {
     "Name" = "${local.vpc_name}-NAT-a"
   }
 }
 
 resource "aws_eip" "nat-b" {
-  vpc  = true
+  vpc = true
   tags = {
     "Name" = "${local.vpc_name}-NAT-b"
   }
